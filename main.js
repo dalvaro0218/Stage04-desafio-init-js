0092bf61279385e9cbb4dae92e44965d12e1f094
dalvaro0218 <dalvaro0218@gmail.com>
2023-06-10 17:58:27 -0300

    Finish code

---
+++ main.js
@@ -0,0 +1,101 @@
+// console.log("Hello World")
+
+// let a = parseInt(prompt("Digite o primeiro Número"))
+// let b = parseInt(prompt("Digite o segundo Número"))
+
+// alert(Number(a + b))
+
+
+
+// let isNumber = 10;
+// console.log(typeof isNumber);
+
+
+
+
+
+// // Declarando a variável
+// let valor = "Oi, tudo bem";
+
+// // Verificando se o valor é uma string
+// if (typeof valor === "string") {
+//     console.log("É uma string");
+// } else {
+//     console.log("Não é uma string");
+// }
+
+
+
+
+
+
+
+// // Declarando a variável
+// let valor = 5;
+
+// // Verificando se o valor é booleano
+// if (typeof valor === "boolean") {
+//     console.log("É um Boolean");
+// } else {
+//     console.log("Não é Bollean");
+// }
+
+
+
+
+
+// let a = 5
+// let b = 7
+
+// console.log(a+b)
+
+
+
+
+
+// let a = 5
+// let b = 7
+
+// console.log(a-b)
+
+
+
+
+
+// let a = 5
+// let b = 7
+
+// console.log(a/b)
+
+
+
+
+
+// let a = 5
+// let b = 7
+
+// console.log(a*b)
+
+
+
+
+// let a = prompt("Digite:")
+
+// if (a % 2 == 0) {
+//     console.log("É Par")
+// } else {
+//     console.log("Não é par")
+// }
+
+
+
+
+
+let a = prompt("Digite:")
+
+if (a % 2 > 0) {
+    console.log("É Impar")
+} else {
+    console.log("Não é impar")
+}
+
